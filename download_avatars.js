282fd3d5bdeae23dd9ed77d38df3073d8e553543
edward eaglesham <edward.eaglesham@gmail.com>
2018-01-23 00:38:58 +0000

    parsed JSON file and added console log of each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -13,11 +13,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
+
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  for (var object of result) {
+    console.log(object.avatar_url);
+  }
 });
